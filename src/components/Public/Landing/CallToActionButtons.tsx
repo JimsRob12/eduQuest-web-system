00499851708fd9430bd72f63bfb3997b1beb70d9
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-09-18 23:21:37 +0800

    Refactor landing page layout and components

--- src/components/Public/Landing/CallToActionButtons.tsx
+++ src/components/Public/Landing/CallToActionButtons.tsx
@@ -5,21 +5,21 @@ import { Button } from "@/components/ui/button";
 export default function CallToActionButtons() {
   return (
     <div className="flex flex-col gap-2 md:flex-row">
-      <NavLink to="/signup">
+      <NavLink to="/explore">
         <Button className="relative flex h-fit w-60 items-center gap-1 rounded-md px-6 py-3 text-lg font-normal shadow-[-4px_4px_0px_#3b1b55] transition-all duration-300 hover:-translate-x-1 hover:translate-y-1 hover:shadow-none dark:shadow-[-4px_4px_0px_#aaa4b1] dark:hover:shadow-none md:w-full">
-          Sign up <ChevronRight />
+          Explore <ChevronRight />
           <img
             src="/arrow-to-sign.png"
             className="absolute -left-12 -top-24 z-10 hidden w-24 rotate-90 md:block"
           />
         </Button>
       </NavLink>
-      <NavLink to="/login">
+      <NavLink to="/faq">
         <Button
           variant="secondary"
           className="flex h-fit w-60 items-center gap-1 rounded-md px-6 py-3 text-lg font-normal shadow-[-4px_4px_0px_#aaa4b1] transition-all duration-300 hover:-translate-x-1 hover:translate-y-1 hover:shadow-none dark:shadow-[-4px_4px_0px_#3b1b55] dark:hover:shadow-none md:w-full"
         >
-          Login <ChevronRight />
+          Learn More <ChevronRight />
         </Button>
       </NavLink>
     </div>
--- src/components/Public/Landing/LandingPage.tsx
+++ src/components/Public/Landing/LandingPage.tsx
@@ -3,9 +3,11 @@ import Header from "./Header";
 
 export default function LandingPage() {
   return (
-    <section className="my-8 flex h-[calc(100%-12rem)] w-full flex-col items-center justify-center space-y-8">
-      <Header />
-      <CallToActionButtons />
-    </section>
+    <>
+      <section className="my-8 flex h-[calc(100%-12rem)] w-full flex-col items-center justify-center space-y-8">
+        <Header />
+        <CallToActionButtons />
+      </section>
+    </>
   );
 }
