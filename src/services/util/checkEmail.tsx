f347e8bed4aedd2b4fdf82834c065321ef09caa9
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-09-27 12:44:24 +0800

    Refactor supabase.ts and remove unused code

---
+++ src/services/util/checkEmail.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import supabase from "../supabase";
+
+// export const checkEmailExists = async (
+//   email: string,
+//   table: string,
+// ): Promise<any | undefined> => {
+//   const { data, error } = await supabase
+//     .from(table)
+//     .select("id")
+//     .eq("email", email)
+//     .single();
+
+//   if (error && error.code !== "PGRST116") {
+//     console.error("Error checking email:", error);
+//     throw new Error("Error checking email.");
+//   }
+
+//   return data;
+// };
